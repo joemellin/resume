ddf035f4d61688de012b06670e381b3383b687f5
Joe Mellin <mellin.joe@gmail.com>
2015-07-26 18:42:24 -0700

    add image validation

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,4 +1,5 @@
 class Painting < ActiveRecord::Base
 	belongs_to :user
     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "36x36>", :large => "1000x1000>"  }
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
